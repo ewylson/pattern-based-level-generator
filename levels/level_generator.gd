7f331ab327aeb77ae53df6e6a08ebc10ff775f2d
Ewilson <52111043+Ev1ls0n@users.noreply.github.com>
2025-06-30 18:45:18 +0300

    [feat] add chunk variants

--- levels/level_generator.gd
+++ levels/level_generator.gd
@@ -1,8 +1,14 @@
 extends Node2D
 
 
-@export_group("Layers")
-@export var ground_layer : TileMapLayer
+enum {
+	CORNER,
+	SIDEWAY,
+	MIDDLE,
+	IMMUTABLE,
+}
+
+
 @export var obstacles_layer : TileMapLayer
 
 @export_group("Chunk Grid")
@@ -10,24 +16,32 @@ extends Node2D
 @export var chunk_grid_padding := Vector2i(2, 2)
 
 
+var __chunk_grid_map : Array[Array] = [
+	[CORNER, SIDEWAY, CORNER],
+	[SIDEWAY, MIDDLE, SIDEWAY],
+	[CORNER, SIDEWAY, CORNER],
+]
+
+
 func _ready() -> void:
-	var patterns : Array[TileMapPattern] = TileMapPatternPacker.unpack(preload("res://tilemap/data/patterns.res"))
-	for start_cell : Vector2i in __get_chunk_start_cells():
-		obstacles_layer.set_pattern(start_cell, patterns.pick_random())
+	var regular_patterns : Array[TileMapPattern] = TileMapPatternPacker.unpack(preload("res://tilemap/data/regular_patterns.res"))
+	var middle_patterns : Array[TileMapPattern] = TileMapPatternPacker.unpack(preload("res://tilemap/data/middle_patterns.res"))
+	for start_cell : Vector2i in __get_chunk_start_cells(CORNER):
+		obstacles_layer.set_pattern(start_cell, regular_patterns.pick_random())
+	for start_cell : Vector2i in __get_chunk_start_cells(SIDEWAY):
+		obstacles_layer.set_pattern(start_cell, regular_patterns.pick_random())
+	for start_cell : Vector2i in __get_chunk_start_cells(MIDDLE):
+		obstacles_layer.set_pattern(start_cell, middle_patterns.pick_random())
 	return
 
 
-func __get_chunk_start_cells() -> Array[Vector2i]:
+func __get_chunk_start_cells(chunk_type: int) -> Array[Vector2i]:
 	var chunk_start_cells : Array[Vector2i]
-	var chunk_grid_size : Vector2i = __get_chunk_grid_size()
-	for row in range(chunk_grid_size.x):
-		for col in range(chunk_grid_size.y):
-			chunk_start_cells.append(__get_start_cell_on_chunk(Vector2i(row, col)))
+	for row in range(__chunk_grid_map.size()):
+		for col in range(__chunk_grid_map[row].size()):
+			if __chunk_grid_map[row][col] == chunk_type:
+				chunk_start_cells.append(__get_start_cell_on_chunk(Vector2i(col, row)))
 	return chunk_start_cells
-
-
-func __get_chunk_grid_size() -> Vector2i:
-	return (ground_layer.get_used_rect().size - chunk_grid_padding) / chunk_size
 
 
 func __get_start_cell_on_chunk(chunk_coords: Vector2i) -> Vector2i:
