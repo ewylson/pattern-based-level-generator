f1f8d4f5865b491410c417102bbaff002ca340cb
Ewilson <52111043+Ev1ls0n@users.noreply.github.com>
2025-07-01 17:41:50 +0300

    [refactor] improve chunk filling logic for the level generator

--- levels/level_generator.gd
+++ levels/level_generator.gd
@@ -16,6 +16,10 @@ enum {
 @export var chunk_grid_padding := Vector2i(2, 2)
 
 
+var __regular_patterns : Array[TileMapPattern] = TileMapPatternPacker.unpack(preload("res://tilemap/data/regular_patterns.res"))
+var __middle_patterns : Array[TileMapPattern] = TileMapPatternPacker.unpack(preload("res://tilemap/data/middle_patterns.res"))
+
+
 var __chunk_grid_map : Array[Array] = [
 	[CORNER, SIDEWAY, CORNER],
 	[SIDEWAY, MIDDLE, SIDEWAY],
@@ -24,24 +28,34 @@ var __chunk_grid_map : Array[Array] = [
 
 
 func _ready() -> void:
-	var regular_patterns : Array[TileMapPattern] = TileMapPatternPacker.unpack(preload("res://tilemap/data/regular_patterns.res"))
-	var middle_patterns : Array[TileMapPattern] = TileMapPatternPacker.unpack(preload("res://tilemap/data/middle_patterns.res"))
-	for start_cell : Vector2i in __get_chunk_start_cells(CORNER):
-		obstacles_layer.set_pattern(start_cell, regular_patterns.pick_random())
-	for start_cell : Vector2i in __get_chunk_start_cells(SIDEWAY):
-		obstacles_layer.set_pattern(start_cell, regular_patterns.pick_random())
-	for start_cell : Vector2i in __get_chunk_start_cells(MIDDLE):
-		obstacles_layer.set_pattern(start_cell, middle_patterns.pick_random())
+	__fill_chunks_with_patterns()
 	return
 
 
-func __get_chunk_start_cells(chunk_type: int) -> Array[Vector2i]:
+func __fill_chunks_with_patterns() -> void:
+	for start_cell : Vector2i in __get_chunk_start_cells([CORNER, SIDEWAY]):
+		obstacles_layer.set_pattern(start_cell, __regular_patterns.pick_random())
+	for start_cell : Vector2i in __get_chunk_start_cells([MIDDLE]):
+		obstacles_layer.set_pattern(start_cell, __middle_patterns.pick_random())
+	return
+
+
+func __get_chunk_start_cells(chunk_types: Array[int]) -> Array[Vector2i]:
 	var chunk_start_cells : Array[Vector2i]
 	for row in range(__chunk_grid_map.size()):
 		for col in range(__chunk_grid_map[row].size()):
-			if __chunk_grid_map[row][col] == chunk_type:
+			if __is_proper_chunk(__chunk_grid_map[row][col], chunk_types):
 				chunk_start_cells.append(__get_start_cell_on_chunk(Vector2i(col, row)))
 	return chunk_start_cells
+
+
+func __is_proper_chunk(chunk: int, chunk_types: Array[int]) -> bool:
+	var result : bool = false
+	for type : int in chunk_types:
+		if chunk == type:
+			result = true
+			break
+	return result
 
 
 func __get_start_cell_on_chunk(chunk_coords: Vector2i) -> Vector2i:
